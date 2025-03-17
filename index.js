1b60fbcba794ed319e4ee14f7fe0951a41f639e4
Ellis Lunayo <ellislunayo@gmail.com>
2025-03-17 22:24:12 +0300

    Maths arithematic lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1 = 31;  
+let num2 = 2;  
+
+// Multiply num1 and num2 and store the result in 'multiply'
+let multiply = num1 * num2; 
+
+let random = Math.floor(Math.random() * 100) + 1;  
+
+
+
+let num3=4;
+let num4 = 6;
+let mod = 4%6;
+
+
+let max=20;
+math.max(5,10,15,20);
+
+
